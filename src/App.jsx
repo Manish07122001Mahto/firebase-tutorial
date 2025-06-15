520453bdfbe6d0045fb98a758da9473d966d69e8
Manish07122001Mahto <manishmahto.8885@gmail.com>
2025-06-15 15:53:44 +0530

    Refactor App component to integrate Firebase Realtime Database for data management

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { firebaseApp } from "../firebase";
+import { getDatabase, set, ref, get, child, onValue } from "firebase/database";
+const db = getDatabase(firebaseApp);
 
 function App() {
+  const [name, setName] = useState(null);
+  const purData = () =>
+    set(ref(db, "root/a/b"), { id: 2, name: "Manish Mahto", gender: "Male" });
+
+  // const getData = get(child(ref(db), "root/a")).then((sanp) => {
+  //   console.log("Data:", sanp.val());
+  // });
+  useEffect(() => {
+    onValue(ref(db, "root/a/b"), (sanp) => setName(sanp.val().name));
+  }, []);
   return (
     <div>
-      <h1>Firebase Tutorial</h1>
+      <h1>Hello {name}</h1>
+      <button onClick={purData}>Set Data</button>
     </div>
   );
 }
