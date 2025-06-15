706aff9f2ae3e3ebac9c31cef8a7ab7f154a1442
Manish07122001Mahto <manishmahto.8885@gmail.com>
2025-06-15 14:18:02 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import Signup from "./pages/Signup.jsx";
-import Signin from "./pages/Signin.jsx";
-
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { firebaseApp } from "../firebase.js";
-const auth = getAuth(firebaseApp);
 
 function App() {
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // yes you are logged in
-        setUser(user);
-      } else {
-        //user logged out
-        setUser(null);
-      }
-    });
-  }, []);
-  if (user === null) {
-    return (
-      <>
-        <Signup />
-        <Signin />
-      </>
-    );
-  }
   return (
     <div>
-      <h1>Hello '{user.email}'</h1>
-      <button
-        onClick={() => {
-          signOut(auth);
-        }}
-      >
-        Logout
-      </button>
+      <h1>Firebase Tutorial</h1>
     </div>
   );
 }
